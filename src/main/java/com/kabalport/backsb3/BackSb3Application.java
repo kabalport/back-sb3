290f61419a162fdb8278fd0c4ff9a79f3a74cfcc
kabalport <kabalport@gmail.com>
2023-11-16 13:00:47 +0900

    Initial commit

---
+++ src/main/java/com/kabalport/backsb3/BackSb3Application.java
@@ -0,0 +1,13 @@
+package com.kabalport.backsb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSb3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kabalport/backsb3/BackSb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabalport.backsb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
